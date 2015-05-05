478357693086fc0ddeb09fa31701233086dd2998
James B Kemp <jamesbkemp@me.com>
2015-05-05 22:40:36 +0100

    company create action and index view working and passing

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,7 @@
 class CompaniesController < ApplicationController
 
   def index
+    @companies = Company.all
   end
 
   def new
@@ -8,7 +9,8 @@ class CompaniesController < ApplicationController
   end
 
   def create
-    render 'new'
+    @company = Company.create(params[:company].permit(:name, :website, :hr_contact, :hr_contact_email))
+    # Company.create({name: 'New Co Ltd', website: 'http://gethr.io/', hr_contact: 'Miss Lady', hr_contact_email: 'hr@gethr.io'})
+    redirect_to '/companies'
   end
-
 end
--- spec/features/company_feature_spec.rb
+++ spec/features/company_feature_spec.rb
@@ -26,3 +26,17 @@ describe 'new company page' do
     expect(page).to have_content 'Company name:'
   end
 end
+
+describe 'creating a company' do
+  it 'adds the company to the restaurants index' do
+    visit '/companies/new'
+    fill_in 'Company name', with: 'New Co Ltd'
+    fill_in 'Web address', with: 'http://gethr.io/'
+    fill_in 'HR contact', with: 'Miss Lady'
+    fill_in 'HR contact email', with: 'hr@gethr.io'
+    click_button 'Create company'
+
+    expect(current_path).to eq '/companies'
+    expect(page).to have_content 'New Co Ltd'
+  end
+end
